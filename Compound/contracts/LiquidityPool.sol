1b2626467ec8e36a5e5b9ab3f6f6766bb9dd3e0f
Victor Campos <victor.campos@cloudwalk.io>
2022-07-14 16:52:43 -0300

    14.07

--- Compound/contracts/LiquidityPool.sol
+++ Compound/contracts/LiquidityPool.sol
@@ -1,13 +1,52 @@
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./ComptrollerInterface.sol";
+import "./CTokenInterface.sol";
+
 contract LiquidityPool {
-    string public message;
+    IERC20 BRLC;
+    CTokenInterface cBRLC;
+    IERC20 bat;
+    CTokenInterface cBat;
+    ComptrollerInterface comptroller;
 
-    constructor(string memory initMessage) {
-        message = initMessage;
-    }
+    constructor(
+        address _BRLC,
+        address _cBRLC, 
+        address _bat, 
+        address _cBat, 
+        address _comptroller ) public{
+            BRLC = _IERC20(_BRLC);
+            cBRLC = CTokenInterface(_cBRLC);
+            bat = IERC20(_bat);
+            cBat = CTokenInterface(_cBat);
+            comptroller = ComptrollerInterface(_comptroller);
+        }
 
-    function update(string memory newMessage) public {
-        message = newMessage;
-    }
+        function invest() external {
+            BRLC.approve(address(cBRLC), 10000);
+            cBRLC.mint(10000);
+        }
+
+        function casOut() external {
+            uint balance = cBRLC.balanceOf(address(this));
+            cBRLC.redeem(balance);
+        }
+
+        function borrow() external {
+            BRLC.approve(address(cBRLC), 10000);
+            cBRLC.mint(10000);
+
+            address[] memory markets = new address[](1);
+            markets[0] = address(cBRLC);
+            comptroller.enterMarkets(markets);
+
+            cBat.borrow(100);
+        }
+
+        function payback() external {
+            bat.approve(address(cBat), 110);
+            cBat.repayBorrow(100);
+        }
 }
